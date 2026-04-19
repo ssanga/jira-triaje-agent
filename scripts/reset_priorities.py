0027d15685e1daec1047b8f18afa48b68d35c2bc
Santiago Sanchez Garcia <ssanga@outlook.com>
2026-04-19 19:41:45 +0200

    ajustado reset_priorities

--- scripts/reset_priorities.py
+++ scripts/reset_priorities.py
@@ -1,3 +1,4 @@
+import sys, os; sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from dotenv import load_dotenv
 from src.jira import reset_issue_types, reset_priorities
 
@@ -5,8 +6,8 @@ load_dotenv()
 
 PROJECT_KEY = "PT"
 
-# print(f"Reseteando tipologías del proyecto '{PROJECT_KEY}' a Task...\n")
-# reset_issue_types(PROJECT_KEY)
+print(f"Reseteando tipologías del proyecto '{PROJECT_KEY}' a Task...\n")
+reset_issue_types(PROJECT_KEY)
 
 print(f"\nReseteando prioridades del proyecto '{PROJECT_KEY}' a Medium...\n")
 reset_priorities(PROJECT_KEY)
